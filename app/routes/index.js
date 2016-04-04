5638baa1b743163e48d83fb506fa1d226694afd3
michael <michael.vang.1985@gmail.com>
2016-04-04 14:28:42 -0700

    add category details component

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return Ember.RSVP.hash({
+      cities: this.store.findAll('city'),
+      categories: this.store.findAll('category'),
+      listings: this.store.findAll('listing')
+    });
+  }
 });
