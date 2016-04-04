794dec0a9bb605bd2a10caf334a6ec5bacb0ea6d
michael <michael.vang.1985@gmail.com>
2016-04-04 10:19:02 -0700

    make adjustment to model properties

--- app/models/category.js
+++ app/models/category.js
@@ -1,5 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+  name: DS.attr(),
+  city: DS.attr(),
+  state: DS.attr(),
+  country: DS.attr(),
+  listings: DS.hasMany('listing', { async: true})
 });
--- app/models/listing.js
+++ app/models/listing.js
@@ -1,5 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+  title: DS.attr(),
+  description: DS.attr(),
+  contact: DS.attr(),
+  category: DS.belongsTo('category', {async: true}),
+  subcategory: DS.attr()
 });
